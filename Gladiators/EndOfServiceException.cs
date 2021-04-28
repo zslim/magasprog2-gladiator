a159d992e2f06cc2240e1c6e400e2b12c5d890c9
Zsofia Limbek <zsofia.limbek@gmail.com>
2021-04-28 10:09:02 +0200

    added exceptions

---
+++ Gladiators/EndOfServiceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Gladiators
+{
+    [Serializable]
+    internal class EndOfServiceException : Exception
+    {
+        public EndOfServiceException()
+        {
+        }
+
+        public EndOfServiceException(string message) : base(message)
+        {
+        }
+
+        public EndOfServiceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected EndOfServiceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Gladiators/IncorrectYearNumberException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Gladiators
+{
+    [Serializable]
+    internal class IncorrectYearNumberException : Exception
+    {
+        public IncorrectYearNumberException()
+        {
+        }
+
+        public IncorrectYearNumberException(string message) : base(message)
+        {
+        }
+
+        public IncorrectYearNumberException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IncorrectYearNumberException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
