4a60c709f4c0d3db29c82d3a03d6a3daf516e057
Zsófia Limbek <zsofia.limbek@cloudera.com>
2021-04-28 09:05:04 +0200

    implemented abstract class Gladiator, except for exceptions...

--- Gladiators/Gladiator.cs
+++ Gladiators/Gladiator.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Gladiators
 {
-    class Gladiator
+    abstract class Gladiator
     {
+        private byte years;
+        public byte Years
+        {
+            get { return years; }
+            private set 
+            { 
+                if (value < 0 || value > 10)
+                {
+                    throw new IncorrectYearNumberException("Number of years must be between 0 and 10");
+                }
+                years = value; 
+            }
+        }
+
+        private OriginType origin;
+        public OriginType Origin
+        {
+            get { return origin; }
+        }
+        
+        private bool isFreed;
+        public bool IsFreed
+        {
+            get { return isFreed; }
+            private set 
+            { 
+                if (Years >= 10 && value == false)
+                {
+                    throw new EndOfServiceException("Gladiator must be freed after 10 years of service");
+                }
+                isFreed = value; 
+            }
+        }
+
+        private bool isAlive;
+        public bool IsAlive
+        {
+            get { return isAlive; }
+        }
+
+        private void Die()
+        {
+            isAlive = false;
+        }
+
+        public Gladiator(byte years, OriginType origin)
+        {
+            Years = years;
+            Origin = origin;
+            IsFreed = false;
+            IsAlive = true;
+        }
+        
+        
     }
 }
--- Gladiators/OriginType.cs
+++ Gladiators/OriginType.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Gladiators
 {
-    class OriginType
+    enum OriginType
     {
+        Gallic, Punic, Roman, Thracian
     }
 }
